18ade0b0dcae639f9d4b5b18f69a615472fb9d8c
rahulbala1799 <rahulbala1799@gmail.com>
2026-01-28 18:26:03 +0000

    Fix ESLint error - escape apostrophe in login page

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
           {/* Footer */}
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <a href="#" className="font-medium text-purple-600 hover:text-purple-500">
                 Contact administrator
               </a>
